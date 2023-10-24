b207c1b335aa828eb0b72100f9f74cf838fe185d
Paul Oosterhoff <pauloosterhoff@gmail.com>
2023-10-24 06:16:37 -0600

    Directory structure

--- app/routes/public.js
+++ app/routes/public.js
@@ -4,6 +4,8 @@ const public_controller = require("../controllers/publicController");
 
 //! PUBLIC ROUTES 
 
+// Index
+router.get("/", public_controller.store_get);
 // API *** READ Product - list
 router.get("/api/product/list", public_controller.api_product_list_get);
 
